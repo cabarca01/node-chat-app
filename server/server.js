5b4cf73b679e63a5dc0f8da07950a92ead38e6a5
Carlos Augusto Abarca Bayona <cabarca@paypal.com>
2017-11-17 15:53:45 +0100

    Adds connect / disconnect handlers

--- server/server.js
+++ server/server.js
@@ -2,10 +2,22 @@ require('./config/config');
 
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+
+// client-server communications
+var io = socketio(server);
+io.on('connection', (socket) => {
+    console.log ('Incomming conection from browser');
+    socket.on('disconnect', () => {
+        console.log ('user disconnected');
+    });
+});
 
 // configure middleware
 app.use('/', express.static(publicPath));
@@ -15,7 +27,7 @@ app.use('/', express.static(publicPath));
 
 // start server
 if(!module.parent) {
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
         console.log(`Server started at port ${process.env.PORT}`);
     });
 }
