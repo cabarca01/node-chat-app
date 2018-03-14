e07621ff64d776a6354a78aeadfa2936532516bf
Carlos Augusto Abarca Bayona <cabarca@paypal.com>
2018-03-14 15:28:14 +0100

    adding formatted time to the messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,27 +10,35 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(data) {
     let li = jQuery('<li></li>');
-    li.text(`${data.from}: ${data.text}`);
+    let formattedTime = moment(data.createdOn).format('h:mm a');
+    li.text(`(${formattedTime}) ${data.from}: ${data.text}`);
     jQuery('#message-list').append(li);
 });
 
 socket.on('newLocationMessage', function(data){
+    let formattedTime = moment(data.createdOn).format('h:mm a');
     let li = jQuery('<li></li>');
     let a = jQuery('<a>My Current Location</a>');
     //prepare link
     a.attr('target', '_blank');
     a.attr('href', data.url );
     //message text
-    li.text(`${data.from}: `);
+    li.text(`(${formattedTime}) ${data.from}: `);
     li.append(a);
     jQuery('#message-list').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+    let button = jQuery('#send-button');
+    let msgTextbox = jQuery('#message');
+    button.attr('disabled', 'disabled').text('Sending...');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('#message').val()
+        text: msgTextbox.val()
+    }, function() {
+        msgTextbox.val('');
+        button.removeAttr('disabled').text('Send');
     });
 });
 
@@ -39,12 +47,17 @@ locationButton.on('click', function(){
     if(!navigator.geolocation) {
         return alert('Your browser doesn\'t support geolocation');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
     navigator.geolocation.getCurrentPosition(function (position){
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             longitude: position.coords.longitude,
             latitude: position.coords.latitude
+        }, function(){
+            jQuery('#message').val('');
         });
     }, function () {
-        return alert('Unable to obtain you current position')
+        alert('Unable to obtain you current position');
+        locationButton.removeAttr('disabled').text('Send Location');
     });
 });
